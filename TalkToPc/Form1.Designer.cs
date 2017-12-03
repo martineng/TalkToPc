74c2d9951bd3cb37d4a1442fc59a428fd4c5227a
martineng <martin.eca.6651@gmail.com>
2017-12-03 16:19:36 +0800

    Initial commit, program was written before git init

---
+++ TalkToPc/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TalkToPc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxInput = new System.Windows.Forms.TextBox();
+            this.tboxOutput = new System.Windows.Forms.TextBox();
+            this.lbInput = new System.Windows.Forms.Label();
+            this.lbOutput = new System.Windows.Forms.Label();
+            this.btnClearTxt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tboxInput
+            // 
+            this.tboxInput.Location = new System.Drawing.Point(39, 90);
+            this.tboxInput.Multiline = true;
+            this.tboxInput.Name = "tboxInput";
+            this.tboxInput.Size = new System.Drawing.Size(257, 164);
+            this.tboxInput.TabIndex = 0;
+            this.tboxInput.TextChanged += new System.EventHandler(this.tboxInput_TextChanged);
+            this.tboxInput.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tboxInput_MouseDoubleClick);
+            // 
+            // tboxOutput
+            // 
+            this.tboxOutput.Location = new System.Drawing.Point(398, 90);
+            this.tboxOutput.Multiline = true;
+            this.tboxOutput.Name = "tboxOutput";
+            this.tboxOutput.Size = new System.Drawing.Size(257, 164);
+            this.tboxOutput.TabIndex = 1;
+            this.tboxOutput.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.tboxOutput_MouseDoubleClick);
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(36, 64);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(54, 13);
+            this.lbInput.TabIndex = 2;
+            this.lbInput.Text = "Text Here";
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.AutoSize = true;
+            this.lbOutput.Location = new System.Drawing.Point(395, 64);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(162, 13);
+            this.lbOutput.TabIndex = 3;
+            this.lbOutput.Text = "Here is what your PC understand";
+            // 
+            // btnClearTxt
+            // 
+            this.btnClearTxt.Location = new System.Drawing.Point(308, 263);
+            this.btnClearTxt.Name = "btnClearTxt";
+            this.btnClearTxt.Size = new System.Drawing.Size(75, 23);
+            this.btnClearTxt.TabIndex = 5;
+            this.btnClearTxt.Text = "Clear Text";
+            this.btnClearTxt.UseVisualStyleBackColor = true;
+            this.btnClearTxt.Click += new System.EventHandler(this.btnClearTxt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Double Click Text Box to Clear text or press the button to clear both";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 298);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClearTxt);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.lbInput);
+            this.Controls.Add(this.tboxOutput);
+            this.Controls.Add(this.tboxInput);
+            this.Name = "Form1";
+            this.Text = "TalkToPc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxInput;
+        private System.Windows.Forms.TextBox tboxOutput;
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.Label lbOutput;
+        private System.Windows.Forms.Button btnClearTxt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TalkToPc/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TalkToPc
+{
+    // This is a simple program that will read user input from keyboard and mouse event
+
+    public partial class Form1 : Form
+    {
+        // Global var storing the text string
+        string text;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Enable KeyPreview, allow program to read your keyboard
+            KeyPreview = true;
+            
+            //Assign action
+            tboxInput.KeyPress += new KeyPressEventHandler(tboxInput_KeyPress);
+            tboxInput.TextChanged += tboxInput_TextChanged;
+        }
+
+        // When any entrey is in the tboxInput
+        private void tboxInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            text += "[" + e.KeyChar.ToString() + "] pressed.\r\n";
+        }
+
+        // When tboxInput changed
+        private void tboxInput_TextChanged(object sender, EventArgs e)
+        {
+            tboxOutput.Text = text;
+        }
+
+        // Just a simple button to clear both text boxes
+        private void btnClearTxt_Click(object sender, EventArgs e)
+        {
+            text = "";
+            tboxOutput.Text = "";
+            tboxInput.Text = "";
+        }
+        
+        // Double click the input textbox to clear text
+        private void tboxInput_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            tboxInput.Text = "";
+            text = "";
+        }
+
+        // Double click the output textbox to clear to text
+        private void tboxOutput_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            tboxOutput.Text = "";
+            text = "";
+        }
+    }
+}
